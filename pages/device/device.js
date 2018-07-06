9533c4a2bda0c2caf04d0500d2ce36e1f9dbf893
itx <>
2018-07-06 16:54:30 +0800

    优化添加资产

--- pages/device/device.js
+++ pages/device/device.js
@@ -312,16 +312,22 @@ Page({
   bindScanClick:function() {
     console.log('照相机拍照')
    var that = this
+   that.setData({
+     modelIndex: null,
+     brandIndex: null,
+     systemVersionIndex: null,
+     companyCode: null,
+     deviceCode: null,
+   })
     //相机拍照
     wx.chooseImage({
       count: 1, // 默认9
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['camera'], // 可以指定来源是相册还是相机，默认二者都有
       success: function (res) {
-        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-        var tempFilePaths = res.tempFilePaths
-        // console.log("图片地址:%s", tempFilePaths)
-        // console.log("ocrSign：%s", that.data.ocrSign)
+
+
+        var tempFilePaths = res.tempFilePaths;
         wx.showLoading({
           title: '资产识别中...',
         })
